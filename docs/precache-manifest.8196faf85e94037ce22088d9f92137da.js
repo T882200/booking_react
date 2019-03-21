90163e4b76ebf0588a602004dd1ec7cfc3182a53
Idan Madar <t882200@gmail.com>
2019-03-21 08:57:56 +0000

    build and deploy to ghpages

---
+++ docs/precache-manifest.8196faf85e94037ce22088d9f92137da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64c7842a11e5e2e02b56",
+    "url": "/static/css/main.d0f6c1ab.chunk.css"
+  },
+  {
+    "revision": "64c7842a11e5e2e02b56",
+    "url": "/static/js/main.9b57dff2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a81fa19b496917c06531",
+    "url": "/static/css/2.3dc216ba.chunk.css"
+  },
+  {
+    "revision": "a81fa19b496917c06531",
+    "url": "/static/js/2.28071d24.chunk.js"
+  },
+  {
+    "revision": "47c1412ec45d1db01594fcb3a0455356",
+    "url": "/index.html"
+  }
+];
